5e4a9143b0daf621739757ac8fd7296936a8c826
Inbavenkat21 <117255366+Inbavenkat21@users.noreply.github.com>
2024-10-27 22:42:48 +0530

    Update api.py

--- api.py
+++ api.py
@@ -3,18 +3,15 @@ from pydantic import BaseModel
 import numpy as np
 from phishingdetection import FeatureExtraction, gbc
 
-# FastAPI app initialization
 app = FastAPI()
 
-# Define request model for API endpoint
 class URLRequest(BaseModel):
     url: str
 
-# FastAPI endpoint for phishing detection
 @app.post("/verify_url")
 async def verify_url(request: URLRequest):
     obj = FeatureExtraction(request.url)
-    x = np.array(obj.getFeaturesList()).reshape(1, 30)
-    y_pred = gbc.predict(x)[0]
+    features = np.array(obj.getFeaturesList()).reshape(1, -1)
+    y_pred = gbc.predict(features)[0]
     result = "safe" if y_pred == 1 else "phishing"
     return {"status": result, "prediction": y_pred}
